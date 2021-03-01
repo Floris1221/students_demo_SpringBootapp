ce6b5034d195f05eb06840daacd8b4ed1c20778f
Florian <florek.gaffke@gmail.com>
2021-03-01 23:24:44 +0100

    App init

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,56 @@
+package com.example.demo.student;
+
+public class Student {
+    private long id;
+    private int age;
+    private String name;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(long id, int age, String name, String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Student(int age, String name, String email) {
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
